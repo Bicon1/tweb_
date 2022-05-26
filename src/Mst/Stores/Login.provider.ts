3be8f147e741f2818a9f8c8d85ad0a3b597cf727
Bicon1 <grosu.victor@iis.utm.md>
2022-05-26 19:00:36 +0300

    Lab8

--- src/Mst/Stores/Login.provider.ts
+++ src/Mst/Stores/Login.provider.ts
@@ -9,8 +9,8 @@ export const loginStore = LoginStore.create({
         },
         {
             id: '2',
-            user: 'Dorel',
-            password: 'dorel123'
+            user: 'Victor',
+            password: 'Victor123'
         }
     ]
 })
